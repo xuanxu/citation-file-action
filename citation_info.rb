23edde01d5e6cdee3a7b6fab5f34fd00f3174ce4
Juanjo Bazán <jjbazan@gmail.com>
2023-03-26 21:57:17 +0200

    Do not propagate errors

--- citation_info.rb
+++ citation_info.rb
@@ -1,40 +1,47 @@
-citation_file_path = ENV["CITATION_FILE_PATH"].to_s
+begin
 
-if !citation_file_path.empty? && File.exist?(citation_file_path)
+  citation_file_path = ENV["CITATION_FILE_PATH"].to_s
 
-  citation_cff_contents = File.read(citation_file_path)
+  if !citation_file_path.empty? && File.exist?(citation_file_path)
 
-  if citation_cff_contents.empty?
-    system("echo 'CITATION.cff file is empty: #{citation_file_path}'")
-  else
-    citation_info_msg = <<~CITATIONINFO
-      **Ensure proper citation** by uploading a plain text CITATION.cff file to the default branch of your repository.
+    citation_cff_contents = File.read(citation_file_path)
 
-      If using GitHub, a *Cite this repository* menu will appear in the About section, containing both APA and BibTeX formats. When exported to Zotero using a browser plugin, Zotero will automatically create an entry using the information contained in the .cff file.
+    if citation_cff_contents.empty?
+      system("echo 'CITATION.cff file is empty: #{citation_file_path}'")
+    else
+      citation_info_msg = <<~CITATIONINFO
+        **Ensure proper citation** by uploading a plain text CITATION.cff file to the default branch of your repository.
 
-      You can copy the contents for your CITATION.cff file here:
+        If using GitHub, a *Cite this repository* menu will appear in the About section, containing both APA and BibTeX formats. When exported to Zotero using a browser plugin, Zotero will automatically create an entry using the information contained in the .cff file.
 
-      <details><summary><strong>CITATION.cff</strong></summary>
-      <p>
+        You can copy the contents for your CITATION.cff file here:
 
-      ```
-      #{citation_cff_contents}
-      ```
+        <details><summary><strong>CITATION.cff</strong></summary>
+        <p>
 
-      </p>
-      </details>
+        ```
+        #{citation_cff_contents}
+        ```
 
-      If the repository is not hosted on GitHub, a .cff file can still be uploaded to set your preferred citation. Users will be able to manually copy and paste the citation.
+        </p>
+        </details>
 
-      Find more information on .cff files [here](https://github.com/citation-file-format/citation-file-format/blob/main/README.md) and [here](https://docs.github.com/en/repositories/managing-your-repositorys-settings-and-features/customizing-your-repository/about-citation-files).
-    CITATIONINFO
+        If the repository is not hosted on GitHub, a .cff file can still be uploaded to set your preferred citation. Users will be able to manually copy and paste the citation.
 
-    File.open('citation_info_msg.txt', 'w') do |f|
-      f.write citation_info_msg
+        Find more information on .cff files [here](https://github.com/citation-file-format/citation-file-format/blob/main/README.md) and [here](https://docs.github.com/en/repositories/managing-your-repositorys-settings-and-features/customizing-your-repository/about-citation-files).
+      CITATIONINFO
+
+      File.open('citation_info_msg.txt', 'w') do |f|
+        f.write citation_info_msg
+      end
+
+      system("gh issue comment #{ENV['ISSUE_ID']} --body-file citation_info_msg.txt")
     end
-
-    system("gh issue comment #{ENV['ISSUE_ID']} --body-file citation_info_msg.txt")
+  else
+    system("echo 'No citation .cff file found at #{citation_file_path}'")
   end
-else
-  system("echo 'No citation .cff file found at #{citation_file_path}'")
+
+rescue Exception => e
+  system("echo '!! Error creating the citation info. #{citation_file_path} : #{e.message}'")
+  system("echo '!! #{e.backtrace}'")
 end
